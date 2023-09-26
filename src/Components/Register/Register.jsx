9b08b8c0ff6675f6246701c04520dc487091e2a3
fadywadea <fwadea3@gmail.com>
2023-09-26 20:00:34 +0300

    commit

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import Style from './Register.module.css'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { RotatingLines } from 'react-loader-spinner'
+
 
 export default function Register() {
-  function registerSubmit(values) {
-    console.log(values);
+  let navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const [isLoading, setisLoading] = useState(false);
+
+  async function registerSubmit(values) {
+    setisLoading(true);
+    let { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/auth/signup`, values)
+      .catch((err) => {
+        setisLoading(false);
+        setError(err.response.data.message)
+      })
+
+    if (data.message === 'success') {
+      navigate('/login')
+    }
   }
 
   // function validate(values) {
@@ -42,7 +59,7 @@ export default function Register() {
     name: Yup.string().min(3, "name minlength is 3").max(10, "name maxlength is 10").required("name is required"),
     email: Yup.string().email("email is invalid").required("email is required"),
     phone: Yup.string().matches(phoneRegExp, "Phone number is not valid").required("phone is required"),
-    password: Yup.string().matches(/^[a-zA-Z]\w{5,8}$/, "must be <br> * Start with a letter (either uppercase or lowercase). * Be between 6 and 9 characters in total. * Can only contain letters (A-Z or a-z) and numbers (0-9)").required("password is required"),
+    password: Yup.string().matches(/^[A-Za-z][A-Za-z0-9]{5,8}$/, "must be * Start with a letter (either uppercase or lowercase). * Be between 6 and 9 characters in total. * Can only contain letters (A-Z or a-z) and numbers (0-9)").required("password is required"),
     rePassword: Yup.string().oneOf([Yup.ref("password")], "re-Password pattern is invalid").required("rePassword is required")
   })
 
@@ -59,7 +76,10 @@ export default function Register() {
   })
   return <>
     <div className="w-75 mx-auto py-5">
+      {error !== null ? <div className="alert alert-danger">{error}</div> : ''};
+
       <h2>Register Now</h2>
+
       <form onSubmit={formik.handleSubmit}>
 
         <label htmlFor='name'>Name:</label>
@@ -82,9 +102,22 @@ export default function Register() {
         <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.rePassword} className='form-control mb-2' id='rePassword' name='rePassword' type="password" />
         {formik.errors.rePassword && formik.touched.rePassword ? <div className="alert alert-danger mt-2 p-2">{formik.errors.rePassword}</div> : ""}
         <div className='d-flex'>
-          <button disabled={!(formik.isValid && formik.dirty)} className={!(formik.isValid && formik.dirty) ? 'btn btn-lg mt-2 ms-auto' : 'btn btn-lg bg-main text-white mt-2 ms-auto'} type='submit'>Register Now</button>
+          {isLoading ? <button className='btn btn-lg bg-main mt-2 ms-auto'>
+            <RotatingLines
+              strokeColor="white"
+              strokeWidth="5"
+              animationDuration="0.5"
+              width="30"
+              visible={true}
+            />
+          </button>
+            : <button disabled={!(formik.isValid && formik.dirty)} className={!(formik.isValid && formik.dirty) ? 'btn btn-lg mt-2 ms-auto' : 'btn btn-lg bg-main text-white mt-2 ms-auto'} type='submit'>Register Now</button>
+          }
         </div>
       </form>
     </div>
   </>
 }
+
+// console.log(err.response.data.message)
+// console.log(data);
