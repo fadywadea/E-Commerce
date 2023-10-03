5e74786fe22ab41e3c7692e15d2b1dac55380705
fadywadea <fwadea3@gmail.com>
2023-10-03 22:31:24 +0300

    commit

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,69 +1,88 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet'
 import { CartContext } from '../../Context/CartContext'
+import { useQuery } from 'react-query';
+import { ThreeDots } from 'react-loader-spinner';
 
 export default function Cart() {
 
-  let { getLoggedUserCart } = useContext(CartContext);
+  let { getLoggedUserCart, removeCartItem } = useContext(CartContext);
   const [cartDetails, setCartDetails] = useState(null);
+
+  let { isLoading } = useQuery('carts', getCart);
+  console.log(isLoading);
+
+
+  async function removeItem(id) {
+    let { data } = await removeCartItem(id);
+    setCartDetails(data);
+  }
 
   async function getCart() {
     let { data } = await getLoggedUserCart();
     setCartDetails(data);
   }
 
+
   useEffect(() => {
     getCart();
-  }, []);
+  });
 
   return <>
-    <Helmet>
-      <meta name="description" content="Web site created using create-react-app" />
-      <meta name="keywords" content="HTML5 CSS3 Bootstrap JS React" />
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#000000" />
-      <title>Cart</title>
-    </Helmet>
-    {cartDetails ?
-
-      <div className='container w-75 my-4 mx-auto py-3 bg-main-light'>
-        <h3>Shopping Cart</h3>
-
-        <div className="d-flex justify-content-between align-items-center mt-3 mb-3">
-          <div>
-            <h4 className='h6 fw-bolder'>total items: <span className='color-numbers fw-bold'>{cartDetails.numOfCartItems}</span> </h4>
-          </div>
-          <div>
-            <h5 className='h6 fw-bolder'>Total Cart Price: <span className='color-numbers fw-bold'>{cartDetails.data.totalCartPrice}</span> EGP</h5>
-          </div>
-        </div>
-
-        {cartDetails.data.products?.map((product) =>
-          <div className='align-items-center row border-bottom py-2 px-2' key={product.product?.id}>
-
-            <div className="col-md-2">
-              <img className='w-100' src={product.product?.imageCover} alt={product?.id} />
-            </div>
-
-            <div className="col-md-10">
-              <div className="d-flex justify-content-between align-items-center">
-
-                <div>
-                  <h3 className='h6'>{product.product?.title.split(" ").slice(0, 3).join(' ')}</h3>
-                  <h4 className='h6 text-main'> Price : {product.price}</h4>
-                </div>
-
-                <div>
-                  <button className='btn btn-outline-success'>+</button>
-                  <span className='mx-2'>{product?.count}</span>
-                  <button className='btn btn-outline-danger'>-</button>
-                </div>
+    {isLoading ?
+      <section className='d-flex justify-content-center align-content-center m-0 p-0'>
+        <ThreeDots
+          height="100vh"
+          width="80"
+          radius="9"
+          color="#4fa94d"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+        />
+      </section>
+      : <section>
+        <Helmet>
+          <meta name="description" content="Web site created using create-react-app" />
+          <meta name="keywords" content="HTML5 CSS3 Bootstrap JS React" />
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <title>Cart</title>
+        </Helmet>
+        {cartDetails ?
+          <div className='container w-75 my-4 mx-auto py-3 bg-main-light'>
+            <h3>Shopping Cart</h3>
+            <div className="d-flex justify-content-between align-items-center mt-3 mb-3">
+              <div>
+                <h4 className='h6 fw-bolder'>total items: <span className='color-numbers fw-bold'>{cartDetails.numOfCartItems}</span> </h4>
               </div>
-              <button className='btn btn-outline-danger font-sm p-1'> <i className=' fas fa-trash-can'></i> Remove</button>
+              <div>
+                <h5 className='h6 fw-bolder'>Total Cart Price: <span className='color-numbers fw-bold'>{cartDetails.data.totalCartPrice}</span> EGP</h5>
+              </div>
             </div>
-          </div>)}
-      </div>
-      : ''}
+            {cartDetails.data.products?.map((product) =>
+              <div className='align-items-center row border-bottom py-2 px-2' key={product.product.id}>
+                <div className="col-md-2 px-0">
+                  <img className='w-100' src={product.product?.imageCover} alt={product?._id} />
+                </div>
+                <div className="col-md-10">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div>
+                      <h3 className='h6'>{product.product?.title.split(" ").slice(0, 3).join(' ')}</h3>
+                      <h4 className='h6 text-main'> Price : {product.price}</h4>
+                    </div>
+                    <div>
+                      <button className='btn btn-outline-success'>+</button>
+                      <span className='mx-2'>{product?.count}</span>
+                      <button className='btn btn-outline-danger'>-</button>
+                    </div>
+                  </div>
+                  <button onClick={() => removeItem(product.product.id)} className='btn btn-outline-danger font-sm p-1'> <i className=' fas fa-trash-can'></i> Remove</button>
+                </div>
+              </div>)}
+          </div> : ''}
+      </section>}
   </>
 }
--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -26,8 +26,16 @@ function getLoggedUserCart() {
     .catch((err) => err)
 }
 
+function removeCartItem(productId) {
+  return axios.delete(`https://ecommerce.routemisr.com/api/v1/cart/${productId}`,
+    {
+      headers: header
+    }).then((response) => response)
+    .catch((erro) => erro)
+};
+
 export default function CartContextProvider(props) {
-  return <CartContext.Provider value={{ addToCart, getLoggedUserCart }}>
+  return <CartContext.Provider value={{ addToCart, getLoggedUserCart, removeCartItem }}>
     {props.children}
   </CartContext.Provider>
 }
