e8df1d70df395865b3a6dc44d4150e75f37ba5c4
fadywadea <fwadea3@gmail.com>
2023-10-22 22:25:32 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
     if (localStorage.getItem('userToken') !== null) {
       setUserToken(localStorage.getItem('userToken'));
     }
-  });
+  },[]);
 
   return <CartContextProvider>
     <CounterContextProvider>
